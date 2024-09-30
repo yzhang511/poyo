83e93f55e7fee72e67e8a152257c05e71a11320e
PPWangyc <ppwangyc@gmail.com>
2024-09-29 19:50:58 -0700

    fix

--- eval_result.py
+++ eval_result.py
@@ -20,19 +20,38 @@ for eid in test_eids:
         if eid in f:
             data = np.load(f, allow_pickle=True).item()
             if 'choice' in f:   
-                choice_res[eid] = data['Block']
+                choice_res[eid] = data['Choice']
             elif 'block' in f:
                 block_res[eid] = data['Block']
             elif 'wheel' in f:
-                wheel_res[eid] = data['Block']
+                wheel_res[eid] = data['Wheel']
             elif 'whisker' in f:
-                whisker_res[eid] = data['Block']
+                whisker_res[eid] = data['Whisker']
             else:
                 raise ValueError('Unknown result type')
-            
+print('Choice / Block / Wheel / Whisker')
+print("Accuracy Result")
+choice_list, block_list, wheel_list, whisker_list = [], [], [], []
 for eid in test_eids:
-    print(f'Choice: {eid}, {choice_res[eid]}')
-    print(f'Block: {eid}, {block_res[eid]}')
-    print(f'Wheel: {eid}, {wheel_res[eid]}')
-    print(f'Whisker: {eid}, {whisker_res[eid]}')
-    print('')
+    try:
+        print(f'{eid}: {round(choice_res[eid]["accuracy"],5)} / {round(block_res[eid]["accuracy"],5)} / {round(wheel_res[eid]["r2_trial"],5)} / {round(whisker_res[eid]["r2_trial"],5)}')
+        choice_list.append(choice_res[eid]["accuracy"])
+        block_list.append(block_res[eid]["accuracy"])
+        wheel_list.append(wheel_res[eid]["r2_trial"])
+        whisker_list.append(whisker_res[eid]["r2_trial"])
+    except:
+        continue
+print(f"Mean: {round(np.mean(choice_list),5)} / {round(np.mean(block_list),5)} / {round(np.mean(wheel_list),5)} / {round(np.mean(whisker_list),5)}")
+
+print("Balanced Accuracy Result")
+choice_list, block_list, wheel_list, whisker_list = [], [], [], []
+for eid in test_eids:
+    try:
+        print(f'{eid}: {round(choice_res[eid]["balanced_accuracy"],5)} / {round(block_res[eid]["balanced_accuracy"],5)} / {round(wheel_res[eid]["r2_trial"],5)} / {round(whisker_res[eid]["r2_trial"],5)}')
+        choice_list.append(choice_res[eid]["balanced_accuracy"])
+        block_list.append(block_res[eid]["balanced_accuracy"])
+        wheel_list.append(wheel_res[eid]["r2_trial"])
+        whisker_list.append(whisker_res[eid]["r2_trial"])
+    except:
+        continue
+print(f"Mean: {round(np.mean(choice_list),5)} / {round(np.mean(block_list),5)} / {round(np.mean(wheel_list),5)} / {round(np.mean(whisker_list),5)}")
