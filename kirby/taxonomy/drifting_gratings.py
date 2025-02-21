e6a61259560773ede851db6874bb306853b094ff
Yizi Zhang <yzhang39@dt-login04.delta.ncsa.illinois.edu>
2025-02-21 16:37:59 -0600

    fix taxonomy

---
+++ kirby/taxonomy/drifting_gratings.py
@@ -0,0 +1,12 @@
+from .core import StringIntEnum
+
+
+class Orientation_8_Classes(StringIntEnum):
+    angle_0 = 0
+    angle_45 = 1
+    angle_90 = 2
+    angle_135 = 3
+    angle_180 = 4
+    angle_225 = 5
+    angle_270 = 6
+    angle_315 = 7
